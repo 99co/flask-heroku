7a83052d7ecbe581df056157dcf10b9830d3c730
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2013-09-21 05:00:39 +0800

    fix(db) use the 'host' param and pass it an URI

--- acme/db.py
+++ acme/db.py
@@ -7,7 +7,7 @@ _db_set = {}
 def DB(uri):
     global _db_set
     if uri not in _db_set:
-        _client = MongoClient(uri)
+        _client = MongoClient(host=uri)
         _db_set[uri] = _client[uri_parser.parse_uri(uri)['database']]
 
     return _db_set[uri]
